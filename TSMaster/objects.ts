69b51bc3c4ec1a806f5d77dcd487120c766a9ccf
Inhoob <inhoob@gmail.com>
2022-11-11 02:34:51 +0900

    objects type

--- TSMaster/objects.ts
+++ TSMaster/objects.ts
@@ -1,5 +1,125 @@
-const dog = {
-  name: "Elton",
-  breed: "Aus",
-  age: 0.5,
+// Objects as parameters:
+function printName(person: { first: string; last: string }): void {
+  console.log(`${person.first} ${person.last}`);
+}
+
+printName({ first: "Thomas", last: "Jenkins" });
+
+const singer = { first: "Mick", last: "Jagger", age: 473, isAlive: true };
+printName(singer);
+
+// let coordinate: { x: number; y: number } = { x: 34, y: 2 };
+
+// function randomCoordinate(): { x: number; y: number } {
+//   return { x: Math.random(), y: Math.random() };
+// }
+
+// function doublePoint(point: { x: number; y: number }): {
+//   x: number;
+//   y: number;
+// } {
+//   return { x: point.x * 2, y: point.y * 2 };
+// }
+
+//Type alias => 타입을 재사용하고 이름을 지정함
+
+// type Point = {
+//   x: number;
+//   y: number;
+// };
+
+let coordinate: Point = { x: 34, y: 2 };
+
+function randomCoordinate(): Point {
+  return { x: Math.random(), y: Math.random() };
+}
+
+function doublePoint(point: Point): Point {
+  return { x: point.x * 2, y: point.y * 2 };
+}
+
+type Song = {
+  title: string;
+  artist: string;
+  numStreams: number;
+  credits: { producer: string; writer: string };
+};
+
+function calculatePayout(song: Song): number {
+  return song.numStreams * 0.0033;
+}
+
+function printSong(song: Song): void {
+  console.log(`${song.title} - ${song.artist}`);
+}
+
+const mySong: Song = {
+  title: "Unchained Melody",
+  artist: "Righteous Brothers",
+  numStreams: 12873321,
+  credits: {
+    producer: "Phil Spector",
+    writer: "Alex North",
+  },
+};
+
+const earnings = calculatePayout(mySong);
+console.log(earnings);
+printSong(mySong);
+
+//optional property
+type Point = {
+  x: number;
+  y: number;
+  z?: number; //optional property
+};
+
+const myPoint: Point = { x: 1, y: 3 }; //z가 없어도 잘 동작함
+
+//readonly = 값을 변경하려고 할 때 경고. 하지만 참조타입은 바꿀 수 있다. 원시타입만 변경 불가.
+type User = {
+  readonly id: number;
+  username: string;
+};
+
+const user: User = {
+  id: 12837,
+  username: "catgurl",
+};
+
+console.log(user.id);
+user.id;
+
+//intersect type
+type Circle = {
+  radius: number;
+};
+
+type Colorful = {
+  color: string;
+};
+
+type ColorfulCircle = Circle & Colorful; //위의 두가지 property를 모두 사용가능
+
+const happyFace: ColorfulCircle = {
+  radius: 4,
+  color: "yellow",
+};
+
+type Cat = {
+  numLives: number;
+};
+type Dog = {
+  breed: string;
+};
+
+type CatDog = Cat &
+  Dog & {
+    age: number;
+  };
+
+const christy: CatDog = {
+  numLives: 7,
+  breed: "Husky",
+  age: 9,
 };
