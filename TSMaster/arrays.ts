8ea95e918a5911e3d3b2634e0e2118214f32860e
Inhoob <inhoob@gmail.com>
2022-11-13 15:37:48 +0900

    array type

---
+++ TSMaster/arrays.ts
@@ -0,0 +1,18 @@
+const activeUsers: string[] = [];
+
+activeUsers.push("demy");
+
+// const bools: Array<boolean> = []; 이 구문과 아래의 구문이 똑같다
+const bools: boolean[] = [];
+
+const coords: Point[] = [];
+coords.push({ x: 23, y: 8, z: 4 });
+// coords.push({ x: 23, y: 8, z: "4" });
+
+//다차원 배열에 적용해보기
+
+const board: string[][] = [
+  ["X", "O", "X"],
+  ["X", "O", "X"],
+  ["X", "O", "X"],
+];
