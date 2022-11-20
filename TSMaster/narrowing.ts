0bd80abd36f72c336ef4f48a139738e0e23187e0
Inhoob <inhoob@gmail.com>
2022-11-20 23:03:24 +0900

    narrowing

---
+++ TSMaster/narrowing.ts
@@ -0,0 +1,169 @@
+// Typeof Narrowing:
+function triple(value: number | string) {
+  if (typeof value === "string") {
+    return value.repeat(3);
+  }
+  return value * 3;
+}
+//typeof로 Guard하면 primitive type은 커버가 가능하다.
+
+//truthyness guard
+const el = document.getElementById("idk");
+if (el) {
+  el;
+} else {
+  el;
+}
+
+// Truthiness Narrowing:
+const printLetters = (word?: string) => {
+  if (word) {
+    for (let char of word) {
+      console.log(char);
+    }
+  } else {
+    console.log("YOU DID NOT PASS IN A WORD!");
+  }
+};
+
+// EQUALITY NARROWING
+function someDemo(x: string | number, y: string | boolean) {
+  if (x === y) {
+    x.toUpperCase();
+  }
+} //double equal은 사용을 자제할것
+
+// IN Operator Narrowing
+interface Movie {
+  title: string;
+  duration: number;
+}
+
+interface TVShow {
+  title: string;
+  numEpisodes: number;
+  episodeDuration: number;
+}
+
+function getRuntime(media: Movie | TVShow) {
+  if ("numEpisodes" in media) {
+    return media.numEpisodes * media.episodeDuration;
+  }
+  return media.duration;
+}
+
+console.log(getRuntime({ title: "Amadeus", duration: 140 }));
+console.log(getRuntime({ title: "Spongebob", numEpisodes: 80, episodeDuration: 30 }));
+
+// Instanceof Narrowing:
+function printFullDate(date: string | Date) {
+  if (date instanceof Date) {
+    console.log(date.toUTCString());
+  } else {
+    console.log(new Date(date).toUTCString());
+  }
+}
+
+// Instanceof Narrowing:
+class User {
+  constructor(public username: string) {}
+}
+class Company {
+  constructor(public name: string) {}
+}
+
+function printName(entity: User | Company) {
+  if (entity instanceof User) {
+    entity;
+  } else {
+    entity;
+  }
+}
+
+// Type Predicates
+
+interface Cat {
+  name: string;
+  numLives: number;
+}
+interface Dog {
+  name: string;
+  breed: string;
+}
+
+//위의 instanceof와 다른점 => instanceof는  Cat이 class라면 씀. 하지만 여기서는 interface
+function isCat(animal: Cat | Dog): animal is Cat {
+  return (animal as Cat).numLives !== undefined;
+}
+
+function makeNoise(animal: Cat | Dog): string {
+  if (isCat(animal)) {
+    animal;
+    return "Meow";
+  } else {
+    animal;
+    return "Woof!";
+  }
+}
+
+// Discriminated Unions => 공통적인 property를 공유하는 여러 유형을 생성하고 해당 property가 리터럴 값이 됨
+
+interface Rooster {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "rooster"; //이런 판별자를 추가해주는 것
+}
+
+interface Cow {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "cow";
+}
+
+interface Pig {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "pig";
+}
+
+interface Sheep {
+  name: string;
+  weight: number;
+  age: number;
+  kind: "sheep";
+}
+
+type FarmAnimal = Pig | Rooster | Cow | Sheep;
+
+function getFarmAnimalSound(animal: FarmAnimal) {
+  switch (animal.kind) {
+    case "pig":
+      return "Oink!";
+    case "cow":
+      return "Moooo!";
+    case "rooster":
+      return "Cockadoodledoo!";
+    case "sheep":
+      return "Baaa!";
+    default:
+      // We should never make it here, if we handled all cases correctly
+      //   const shouldNeverGetHere: never = animal;
+      //   return shouldNeverGetHere
+
+      //보통 이걸로 체크한다. 마약 animal 이 interface에 없다면 에러
+      const _exhaustiveCheck: never = animal;
+      return _exhaustiveCheck;
+  }
+}
+
+const stevie: Rooster = {
+  name: "Stevie Chicks",
+  weight: 2,
+  age: 1.5,
+  kind: "rooster",
+};
+
+console.log(getFarmAnimalSound(stevie));
